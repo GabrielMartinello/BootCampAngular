84b7cc54af83faf764dc44208954dea12df42554
GabrielMartinello <martinellogabriel@gmail.com>
2021-02-26 17:57:44 -0300

    Criando a listagem de usuario

--- src/app/usuarios/usuario-list/usuarios/usuarios.component.ts
+++ src/app/usuarios/usuario-list/usuarios/usuarios.component.ts
@@ -1,5 +1,6 @@
 import {Component, Input, OnChanges, OnInit, SimpleChanges} from '@angular/core';
 import {Usuario} from 'src/app/usuarios/usuario/usuario';
+import {Anuncio} from 'src/app/anuncios/anuncio/anuncio';
 
 @Component({
   selector: 'app-usuarios',
@@ -15,12 +16,17 @@ export class UsuariosComponent implements OnInit, OnChanges {
   ngOnInit(): void {
   }
 
-  ngOnChanges(changes: SimpleChanges): void {
+  ngOnChanges(changes: SimpleChanges): void{
+    if (changes.usuarios){
+      this.rows = this.groupColumns(this.usuarios);
+    }
   }
 
-  // ngOnChanges(changes: SimpleChanges): void{
-  //   if (changes.usuarios){
-  //     this.rows = this.groupColumns(this.usuarios);
-  //   }
-  // }
+  groupColumns(usuarios: Usuario[]): any[]{
+    const newRows = [];
+    for (let i = 0; i < usuarios.length; i += 4){
+      newRows.push(usuarios.slice(i, i + 4));
+    }
+    return newRows;
+  }
 }
